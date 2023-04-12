d9b204b497e3c416bae364e01d25c770f22130ec
zpc <zip_city@163.com>
2023-04-12 14:13:05 +0800

    add a test.py:The form data was successfully sent to the internal server

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import pymysql
 
 app = Flask(__name__)
@@ -6,26 +6,23 @@ app = Flask(__name__)
 from db_account_config import db_kwargs
 connection = pymysql.connect(**db_kwargs)
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', )
 def login():
-    username = request.form['username']
-    password = request.form['password']
+    # username = request.form['username']
+    # password = request.form['password']
 
-    sql = "SELECT Password FROM User WHERE Username=%s"
+    # sql = "SELECT Password FROM User WHERE Username=%s"
 
-    cursor = connection.cursor()
-    cursor.execute(sql, (username))
-    data = cursor.fetchone()
-    connection.commit()
+    # cursor = connection.cursor()
+    # cursor.execute(sql, (username))
+    # data = cursor.fetchone()
+    # connection.commit()
 
-    if data[0] == password:
-        return 'status: success'
-    else:
-        return 'status: failed'
+    # if data[0] == password:
+    #     return 'status: success'
+    # else:
+    #     return 'status: failed'
+    return render_template('form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
