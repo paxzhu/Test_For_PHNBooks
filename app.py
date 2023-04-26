7628bc7c8df22692c4a78880b5ed583beaa08bad
zpc <zip_city@163.com>
2023-04-27 03:33:46 +0800

    update

--- app.py
+++ app.py
@@ -24,25 +24,34 @@ def login():
 @app.route('/read')
 def read():
     title = request.args['title']
-    
+    print(title)
     sql = "SELECT * FROM Notes WHERE Title=%s"
     cursor = connection.cursor()
     cursor.execute(sql, title)
-    article = cursor.fetchone()
-    if not article:
-        return "No article found for title: " + title, 404
-    # return article[1]
-    title, content = article
-    return json.dumps({'title':title, 'content':content})
+    data = cursor.fetchone()
+    article = dict(status = False, content = None)
+    if data:
+        article['status'], article['content'] = True, data[1]
+    return json.dumps(article)
 
 @app.route('/edit')
 def edit(article):
-
+    title = request.form['title']
+    content = request.form['content']
+    sql = "INSERT INTO Notes(Title, Content) Values(%s, %s)"
+    cursor = connection.cursor()
+    cursor.execute(sql, (title, content))
+    cursor.commit()
     pass
 
 @app.route('/overview')
 def overview():
-    pass
+    sql = "SELECT TiTle FROM Notes"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    titles = cursor.fetchall()
+    print(titles)
+    return titles
 
 
 if __name__ == '__main__':
--- front.py
+++ front.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import requests
+from urllib.parse import quote
 app = Flask(__name__)
 server = "http://127.0.0.1:6000"
 @app.route('/')
@@ -32,21 +33,28 @@ def notes():
 @app.route('/read/<title>')
 def read(title):
     print(title)
-    url = server + '/read?title=%s' % title
+    url = server + '/read?title=' + quote(title)
     response = requests.get(url)
     article = response.json()
     print(article)
-    return render_template('read.html', title = article['title'], content = article['content'])
+    if not article['status']:
+        return "No article found for title: " + title, 404
+    return render_template('read.html', title = title, content = article['content'])
 
 @app.route('/edit/<article>')
 def edit(article):
-
+    form = request.form
+    url = server + '/edit'
+    response = requests.post(url, form)
+    status = response.text
     pass
 
 @app.route('/overview')
 def overview():
-    pass
-
+    url = server + '/overview'
+    response = requests.get(url)
+    titles = response.text
+    return render_template()
 
 
 if __name__ == '__main__':
